a64c42d5e5c29e826ee96c622074de245221f225
Onorio Catenacci <catenacci@ieee.org>
2022-08-09 20:41:46 -0400

    Fixes
    I fixed an incorrect exclude directory and added some unit tests to the RPL file.

--- exack.sh
+++ exack.sh
@@ -4,7 +4,7 @@
 # By default skip the dependencies and test directories
 # Thanks to @muru from Linux and Unix stackexchange for helping me with the right shell script for this!
 # https://unix.stackexchange.com/a/710968/440444
-ignore_dirs=("dependencies" "test" "config")
+ignore_dirs=("deps" "test" "config")
 ign_dirs=()
 for i in "${ignore_dirs[@]}"; do
   ign_dirs+=("--ignore-dir=$i");
@@ -12,7 +12,7 @@ done
 
 
 # By default skip the mix.exs file
-ignore_files=("is:mix.exs" "is:credo.exs")
+ignore_files=("is:mix.exs" "is:credo.exs" "is:formatter.exs")
 ign_files=()
 for i in "${ignore_files[@]}"; do
   ign_files+=("--ignore-file=$i");
